39a60c90325dc83d5b5bb180b6ed7aa8d2ecb6e0
vorning <82236004+vorning@users.noreply.github.com>
2025-04-10 13:38:05 +0200

    Deployed website

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import "./styles/components/styles.css"; // Unified styles
 import "./styles/components/loading.css";
 
